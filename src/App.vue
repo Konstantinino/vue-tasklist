8d844a33257bf25d485cdd21c26d5455dd2c5bae
Konstantinin <kostyaaliev4974@gmail.com>
2025-10-24 16:40:56 +0500

    first commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { ref } from 'vue'
+
+const number = ref(5)
+
+const increment = () => number.value++
+</script>
+
+<template>
+  <h1>You did it!</h1>
+  <p>
+    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
+    documentation
+  </p>
+
+  <button @click="increment">Типо счётчик: {{ number }}</button>
+     <h1> hello</h1>
+</template>
+
+<style scoped></style>
